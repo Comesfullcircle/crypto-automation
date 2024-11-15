9eeee393542894c87ebfe6eaa7df7ae2fa00137a
윤설이 <olerlmin1234@gmail.com>
2024-11-15 14:23:52 +0900

    SpringApplication.exit(context); // context로 변경

--- src/main/java/com/example/cryptoautomation/CryptoAutomationApplication.java
+++ src/main/java/com/example/cryptoautomation/CryptoAutomationApplication.java
@@ -2,13 +2,13 @@ package com.example.cryptoautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CryptoAutomationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CryptoAutomationApplication.class, args);
-        SpringApplication.exit(run);
+        ConfigurableApplicationContext context = SpringApplication.run(CryptoAutomationApplication.class, args);
+        SpringApplication.exit(context); // context로 변경
     }
-
 }
